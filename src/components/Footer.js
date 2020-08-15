3be772615dbd4356f1a680492c7258b3f634c79d
mikenath223 <michgoldennathan17@gmail.com>
2020-08-15 22:17:34 +0100

    Style top section on landing page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,39 +3,41 @@ import styles from '../styles/footer.modules.css';
 import footerLogo from '../images/manual-logo-footer.svg';
 
 const Footer = () => (
-  <div className={styles.footer}>
-    <img src={footerLogo} alt="manual-brand-logo" className={styles.logo} />
-    <div className={styles.footerMenu}>
-      <ul className={styles.footerGroupLinks}>
-        <li>PRODUCT</li>
-        <li>Popular</li>
-        <li>Trending</li>
-        <li>Guided</li>
-        <li>Products</li>
-      </ul>
-      <ul className={styles.footerGroupLinks}>
-        <li>PRODUCT</li>
-        <li>Popular</li>
-        <li>Trending</li>
-        <li>Guided</li>
-        <li>Products</li>
-      </ul>
-      <ul className={styles.footerGroupLinks}>
-        <li>PRODUCT</li>
-        <li>Popular</li>
-        <li>Trending</li>
-        <li>Guided</li>
-        <li>Products</li>
-      </ul>
-      <ul className={styles.footerGroupLinks}>
-        <li>FOLLOW US</li>
-        <div className={styles.socialLinks}>
-          <img src={require('../images/facebook.svg')} alt="..." />
-          <img src={require('../images/twitter.svg')} alt="..." />
-          <img src={require('../images/linkedin.svg')} alt="..." />
-        </div>
-      </ul>
-    </div>
+  <div>
+    <footer className={styles.footer}>
+      <img src={footerLogo} alt="manual-brand-logo" className={styles.logo} />
+      <div className={styles.footerMenu}>
+        <ul className={styles.footerGroupLinks}>
+          <li>PRODUCT</li>
+          <li>Popular</li>
+          <li>Trending</li>
+          <li>Guided</li>
+          <li>Products</li>
+        </ul>
+        <ul className={styles.footerGroupLinks}>
+          <li>PRODUCT</li>
+          <li>Popular</li>
+          <li>Trending</li>
+          <li>Guided</li>
+          <li>Products</li>
+        </ul>
+        <ul className={styles.footerGroupLinks}>
+          <li>PRODUCT</li>
+          <li>Popular</li>
+          <li>Trending</li>
+          <li>Guided</li>
+          <li>Products</li>
+        </ul>
+        <ul className={styles.footerGroupLinks}>
+          <li>FOLLOW US</li>
+          <div className={styles.socialLinks}>
+            <img src={require('../images/facebook.svg')} alt="..." />
+            <img src={require('../images/twitter.svg')} alt="..." />
+            <img src={require('../images/linkedin.svg')} alt="..." />
+          </div>
+        </ul>
+      </div>
+    </footer>
   </div>
 );
 
